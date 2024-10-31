a6e91d0c195f73483aed1050e1704c8674bb647b
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-31 21:24:16 +0800

    last commit

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,20 +1,20 @@
-import getRating from "../rating";
+import getRating from '../rating';
 
-test("testing rating function", () => {
-  const dataList = [
-    { name: "мечник", health: 10 },
-    { name: "странник", health: 10 },
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-  ];
+test('testing rating function', () => {
+    const dataList = [
+        { name: 'мечник', health: 10 },
+        { name: 'странник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+    ];
 
-  const expected = [
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-    { name: "мечник", health: 10 },
-    { name: "странник", health: 10 },
-  ];
+    const expected = [
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+        { name: 'странник', health: 10 },
+    ];
 
-  const result = getRating(dataList);
-  expect(result).toEqual(expected);
+    const result = getRating(dataList);
+    expected(result).toEqual(expected);
 });
